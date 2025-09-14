086f0e0226976a38c310982e9c246720d2506fe4
Nishant Raj <nishantraj7479@gmail.com>
2025-09-14 17:50:59 +0530

    Added custom 404 redirect page with loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -9,27 +9,22 @@ import Reports from "./pages/Reports";
 import Header from "./components/Header";
 import AddCustomer from "./pages/AddCustomer";
 import ThermalBill from "./pages/ThermalBill";
-
 import InvoicesPage from "./pages/InvoicesPage";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/Billing" element={<Billing />} />
-        <Route path="/Reports" element={<Reports />} />
-        <Route path="/Header" element={<Header />} />
-        <Route path="/add-customer" element={<AddCustomer />} />
-        <Route path="/thermal-bill/:invoiceId" element={<ThermalBill />} />
-
-        <Route path="/invoicepages" element={<InvoicesPage />}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/billing" element={<Billing />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/header" element={<Header />} />
+      <Route path="/add-customer" element={<AddCustomer />} />
+      <Route path="/thermal-bill/:invoiceId" element={<ThermalBill />} />
+      <Route path="/invoicepages" element={<InvoicesPage />} />
+    </Routes>
   );
 }
-
 
 export default App;
